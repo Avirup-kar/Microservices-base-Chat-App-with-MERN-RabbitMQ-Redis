c7167fc1dfe6eadebb3e6901c1477e6dd851cb80
Avirup-kar <avirupk57@gmail.com>
2026-01-24 23:56:14 +0530

    add

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
 const port = 3000
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
