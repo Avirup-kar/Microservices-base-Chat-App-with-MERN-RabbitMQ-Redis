43168831ed0663ccb45e477c9e5351a00f4a2ebc
Avirup-kar <avirupk57@gmail.com>
2026-01-25 19:32:00 +0530

    add again

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from 'redis';
+import userRoutes from "./routes/user.js";
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+connectDb();
+export const redisCliend = createClient({
+    url: process.env.REDIS_URL,
+});
+redisCliend.connect().then(() => console.log("connected to redis")).catch(console.error);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.use('/api/v1', userRoutes);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,12 +1,27 @@
 import express from "express"
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import {createClient} from 'redis'
+import userRoutes from "./routes/user.js";
 
-const app = express()
-const port = 3000
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+connectDb();
+
+export const redisCliend = createClient({
+  url: process.env.REDIS_URL!,
 })
 
+redisCliend.connect().then(() => console.log("connected to redis")).catch(console.error);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.use('/api/v1', userRoutes);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
