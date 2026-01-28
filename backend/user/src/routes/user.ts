281ac39d5434e083d8b7feb868f35c6a02d8e30c
Avirup-kar <avirupk57@gmail.com>
2026-01-28 21:54:45 +0530

    change again

--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { loginUser, verifyUser } from "../controllers/user.js";
+import { getAllUsers, getAUser, loginUser, myProfile, updateNmae, verifyUser } from "../controllers/user.js";
+import { isAuth } from "../middleware/isAuth.js";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/login", loginUser);
 userRoutes.post("/verify", verifyUser);
+userRoutes.get("/me", isAuth, myProfile);
+userRoutes.get("/user/all", isAuth, getAllUsers);
+userRoutes.get("/user/:id", getAUser);
+userRoutes.post("/update/user", isAuth, updateNmae);
 
 export default userRoutes;
