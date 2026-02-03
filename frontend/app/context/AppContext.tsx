53708585c16fd34fd7aa5e66b9c40efe3c1a215f
Avirup-kar <avirupk57@gmail.com>
2026-02-03 17:25:55 +0530

    add again

--- frontend/app/context/AppContext.tsx
+++ frontend/app/context/AppContext.tsx
@@ -1,50 +1,76 @@
-"use client"
+"use client";
 
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 export const user_service = "http://localhost:5000";
 export const chat_service = "http://localhost:5002";
 
 export interface User {
- _id: string;
- name: string;
- email: string;
+  _id: string;
+  name: string;
+  email: string;
 }
 
-export interface Chat{
- _id: string;
- users: string[];
- latestMessage: {
-   text: string;
-   sender: string;
- };
- createdAt: string;
- updatedAt: string;
- unseenCount?: number;
+export interface Chat {
+  _id: string;
+  users: string[];
+  latestMessage: {
+    text: string;
+    sender: string;
+  };
+  createdAt: string;
+  updatedAt: string;
+  unseenCount?: number;
 }
 
-export interface Chats{
- _id: string;
- user: User;
- chat: Chat;
+export interface Chats {
+  _id: string;
+  user: User;
+  chat: Chat;
 }
 
 interface AppContextType {
- user: User | null;
- loading: boolean;
- isAuth: boolean;
- setUser: React.Dispatch<React.SetStateAction<User | null>>;
- setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+  user: User | null;
+  loading: boolean;
+  isAuth: boolean;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const AppContext = createContext<AppContextType | undefined>(undefined)
+const AppContext = createContext<AppContextType | undefined>(undefined);
 
 interface AppProviderProps {
- children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
-   const [user, setUser] = useState<User | null>(null);
-   const [isAuth, setIsAuth] = useState(false);
-   const [loading, setLoading] = useState(true);
-}
+  const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.get(`${user_service}/api/v1/me`);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <AppContext.Provider value={{ user, setUser, isAuth, setIsAuth, loading }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppData must be used within AppProvider");
+  }
+  return context;
+};
